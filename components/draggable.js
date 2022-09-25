51539df545bf62e56924973f1d8576c592bb5d46
Devin Lizardi <dlizardi@berkeley.edu>
2022-09-25 14:42:34 -0400

    setting up for movability

--- components/draggable.js
+++ components/draggable.js
@@ -1,31 +1,45 @@
-'use strict';
+"use strict"
 
-const e = React.createElement;
+const e = React.createElement
 
 class LikeButton extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = {held: false}
+    super(props)
+    this.state = { held: false }
   }
 
   transformStyle = {
-    backgroundColor: "blanchedalmond"
+    backgroundColor: "blanchedalmond",
+    left: 0,
+    top: 0,
   }
 
   handleClick = () => {
-    this.setState({held: !this.state.held})
+    this.setState({ held: !this.state.held })
+  }
+
+  handleMouseMove = (event) => {
+    this.setState({ x: event.pageX })
+    this.setState({ y: event.pageY })
+    console.log(this.state.x, this.state.y)
   }
 
   render() {
     return (
-      <div className="drag-item" onClick={this.handleClick} style={this.transformStyle}>
-        <br /><br />
+      <div
+        className="drag-item"
+        onClick={this.handleClick}
+        style={this.state.held ? this.updatingPos : this.transformStyle}
+        onMouseMove={this.handleMouseMove}
+      >
+        <br />
+        <br />
         {this.state.held ? "held" : "dropped"}
       </div>
-    );
+    )
   }
 }
 
-const domContainer = document.querySelector('#draggable');
-const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton));
+const domContainer = document.querySelector("#draggable")
+const root = ReactDOM.createRoot(domContainer)
+root.render(e(LikeButton))
