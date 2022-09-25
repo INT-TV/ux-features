ab3cfb4f9a87563ac957e9ff7126d330a058973d
Devin Lizardi <dlizardi@berkeley.edu>
2022-09-25 13:02:34 -0400

    non-functional click and hold capabilities

--- components/draggable.js
+++ components/draggable.js
@@ -5,18 +5,19 @@ const e = React.createElement;
 class LikeButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { liked: false };
+    this.state = {held: false}
   }
 
   handleClick = () => {
-    this.setState({liked: !this.state.liked})
+    this.setState({held: !this.state.held})
   }
 
   render() {
     return (
-      <button onClick={this.handleClick}>
-        {this.state.liked ? "liked" : "not liked"}
-      </button>
+      <div className="drag-item" onClick={this.handleClick}>
+        <br /><br />
+        {this.state.held ? "held" : "dropped"}
+      </div>
     );
   }
 }
