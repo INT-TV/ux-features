4fd5247d6495539d88d33976f5e4c4994dd271df
Devin Lizardi <dlizardi@berkeley.edu>
2022-09-28 13:23:44 -0400

    setting up draggable

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,10 +3,10 @@ function App() {
     <div className="App">
       <h1>INT.TV UX Designs</h1>
       <div className="testing-container">
-
+        <div className="draggable-container" />
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
