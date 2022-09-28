baf25516ecb63c367b68901bfb02dcbb7b595e6b
Devin Lizardi <dlizardi@berkeley.edu>
2022-09-28 19:44:23 -0400

    refactoring draggable

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,38 +1,16 @@
 import React from 'react'
-import Draggable from './components/draggable'
+import Draggable from './components/TextContent'
 
 function App() {
-  const mousePosition = useMousePosition();
 
   return (
     <div className="App">
       <h1>INT.TV UX Designs</h1>
       <div className="testing-container">
-        <Draggable mouseX={mousePosition.x} mouseY={mousePosition.y}/>
+        <Draggable />
       </div>
     </div>
   )
 }
-
-const useMousePosition = () => {
-  const [
-    mousePosition,
-    setMousePosition
-  ] = React.useState({ x: null, y: null });
-
-  React.useEffect(() => {
-    const updateMousePosition = ev => {
-      setMousePosition({ x: ev.clientX, y: ev.clientY });
-    };
-    
-    window.addEventListener('mousemove', updateMousePosition);
-
-    return () => {
-      window.removeEventListener('mousemove', updateMousePosition);
-    };
-  }, []);
-
-  return mousePosition;
-};
 
 export default App
