58556c6a327b194d6a2bbd1749f85521bbdaf2e7
Devin Lizardi <dlizardi@berkeley.edu>
2022-10-10 22:48:50 -0400

    breaking: array change

--- front-end/src/example/videos.js
+++ front-end/src/example/videos.js
@@ -1,7 +1,7 @@
-const exampleVideos = {
+const exampleVideos = [
   test: "https://www.youtube.com/embed/C0DPdy98e4c",
   tiktoks: "https://www.youtube.com/embed/Za5MR4diS60",
   music: "https://www.youtube.com/embed/ors0wpcVDcc"
-}
+]
 
 module.exports = { exampleVideos }
