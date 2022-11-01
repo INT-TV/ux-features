39b397b0487c9f40107f599b7b7e5856a6e7cdbb
Devin Lizardi <dlizardi@berkeley.edu>
2022-11-01 15:04:13 -0700

    feat: adding twich oauth2

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,27 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cookieSession from "cookie-session";
+import passport from "passport";
+import twitchPassport from "passport-twitch-latest";
 import testsRouter from "./routes/testsRouter";
 
 dotenv.config();
+const twichStrategy = twitchPassport.Strategy;
+
 const app = express();
 const PORT = process.env.PORT;
 
+// Middlewares
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cookieSession({secret:"somesecrettokenhere"}));
+app.use(passport.initialize());
+app.use(express.static("./public"));
 
 app.use("/ping", testsRouter);
 
