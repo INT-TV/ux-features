84efdffd37a232c2006257145ec7a12ced7c0844
Devin Lizardi <dlizardi@berkeley.edu>
2022-11-01 15:30:29 -0700

    fix: removed user serialization

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import session from 'express-session';
+import session from "express-session";
 import cors from "cors";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
@@ -15,46 +15,52 @@ const app = express();
 const PORT = process.env.PORT;
 
 // Define our constants, you will change these with your own
-const TWITCH_CLIENT_ID: string | undefined = process.env.TWITCH_CLIENT_ID;
-const TWITCH_SECRET: string | undefined    = process.env.TWITCH_SECRET;
-const SESSION_SECRET: string | undefined   = process.env.SESSION_SECRET;
-const CALLBACK_URL: string | undefined     = 'http://localhost:3000/auth/twitch/callback';  // You can run locally with - http://localhost:3000/auth/twitch/callback
-
+const TWITCH_CLIENT_ID = process.env.TWITCH_CLIENT_ID;
+const TWITCH_SECRET = process.env.TWITCH_SECRET;
+const SESSION_SECRET = process.env.SESSION_SECRET;
+const CALLBACK_URL = "http://localhost:3000/auth/twitch/callback"; // You can run locally with - http://localhost:3000/auth/twitch/callback
 
 // Middlewares
 app.use(express.json());
 app.use(cors());
-app.use(session({secret: SESSION_SECRET, resave: false, saveUninitialized: false}));
+app.use(
+  session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false })
+);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cookieSession({secret:"somesecrettokenhere"}));
+app.use(cookieSession({ secret: "somesecrettokenhere" }));
 app.use(passport.initialize());
 app.use(express.static("./public"));
 
 // passport config
-passport.use(new twitchStrategy({
-  clientID: TWITCH_CLIENT_ID,
-  clientSecret: TWITCH_SECRET,
-  callbackURL: CALLBACK_URL,
-  scope: "user_read"
-},
-function(accessToken, refreshToken, profile, done) {
-  console.log("passport accessing");
-  console.log(`accessToken: ${accessToken} refresh: ${refreshToken} profile: ${profile} done: ${done}`);
-  
-  // User.findOrCreate({ twitchId: profile.id }, function (err, user) {
-  //   return done(err, user);
-  // });
-}
-));
+passport.use(
+  new twitchStrategy(
+    {
+      clientID: TWITCH_CLIENT_ID,
+      clientSecret: TWITCH_SECRET,
+      callbackURL: CALLBACK_URL,
+      scope: "user_read",
+    },
+    function (accessToken: unknown, refreshToken: unknown, profile: unknown, done: unknown) {
+      console.log("passport accessing");
+      console.log(
+        `accessToken: ${accessToken} refresh: ${refreshToken} profile: ${profile} done: ${done}`
+      );
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
+      // User.findOrCreate({ twitchId: profile.id }, function (err, user) {
+      //   return done(err, user);
+      // });
+    }
+  )
+);
 
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
+// passport.serializeUser(function (user, done) {
+//   done(null, user);
+// });
+
+// passport.deserializeUser(function (user, done) {
+//   done(null, user);
+// });
 
 app.use("/ping", testsRouter);
 
